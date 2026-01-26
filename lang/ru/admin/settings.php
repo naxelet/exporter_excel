2170d45952d08099d532bc6fbf64cb7bef00f8e2
Aleksandr Egorov <aleks.egorow2010@yandex.ru>
2026-01-26 20:12:59 +0300

    Add settings admin menu

---
+++ lang/ru/admin/settings.php
@@ -0,0 +1,16 @@
+<?php
+$MESS['AKATAN_EXCEL_SETTINGS_TITLE'] = 'Настройки модуля';
+$MESS['AKATAN_EXCEL_NOT_INSTALLED'] = 'Модуль не установлен';
+$MESS['AKATAN_EXCEL_ACCESS_DENIED'] = 'Доступ запрещен';
+$MESS['AKATAN_EXCEL_TAB_MAIN'] = 'Основные настройки';
+$MESS['AKATAN_EXCEL_TAB_MAIN_TITLE'] = 'Основные параметры модуля';
+$MESS['AKATAN_EXCEL_TAB_PERMISSIONS'] = 'Права доступа';
+$MESS['AKATAN_EXCEL_TAB_PERMISSIONS_TITLE'] = 'Настройка прав доступа к модулю';
+$MESS['AKATAN_EXCEL_SETTING_1'] = 'Настройка 1';
+$MESS['AKATAN_EXCEL_SETTING_2'] = 'Настройка 2';
+$MESS['AKATAN_EXCEL_YES'] = 'Да';
+$MESS['AKATAN_EXCEL_NO'] = 'Нет';
+$MESS['AKATAN_EXCEL_SETTINGS_SAVED'] = 'Настройки сохранены';
+$MESS['AKATAN_EXCEL_SAVE'] = 'Сохранить';
+$MESS['AKATAN_EXCEL_RESTORE_DEFAULTS'] = 'Восстановить по умолчанию';
+$MESS['AKATAN_EXCEL_PERMISSIONS_INFO'] = 'Настройте права доступа для различных групп пользователей';
